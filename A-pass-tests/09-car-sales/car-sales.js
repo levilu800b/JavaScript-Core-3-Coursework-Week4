b4a1acb66c34013288d06fa56f19470601413f90
levilu800b <levilu800b@gmail.com>
2022-11-22 20:17:26 +0000

    09 cars- sales completed

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,59 @@
-function sales(carsSold) {}
+// first solution
+
+// function sales(carsSold) {
+//   let total = {};
+
+//   carsSold.forEach((car) => {
+//     total[car.make]
+//       ? (total[car.make] += car.price)
+//       : (total[car.make] = car.price);
+//   });
+//   return total;
+// }
+
+// second solution
+
+// function sales(carsSold) {
+//     let total = {};
+
+//     carsSold.forEach((car) => {
+//         if (total[car.make]) {
+//             total[car.make] += car.price;
+//         } else {
+//             total[car.make] = car.price;
+//         }
+//     });
+//     return total;
+// }
+
+// third solution
+
+// function sales(carsSold) {
+//     let total = {};
+
+//     for (let car of carsSold) {
+//         if (total[car.make]) {
+//             total[car.make] += car.price;
+//         } else {
+//             total[car.make] = car.price;
+//         }
+//     }
+//     return total;
+// }
+
+// fourth solution
+
+function sales(carsSold) {
+    let total = {};
+
+    for (let car = 0; car < carsSold.length; car++) {
+        if (total[carsSold[car].make]) {
+            total[carsSold[car].make] += carsSold[car].price;
+        } else {
+            total[carsSold[car].make] = carsSold[car].price;
+        }
+    }
+    return total;
+}
 
 module.exports = sales;
