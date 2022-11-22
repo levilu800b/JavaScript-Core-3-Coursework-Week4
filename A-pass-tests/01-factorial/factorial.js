591f440a360a71357904c27aa023204a2c477259
levilu800b <levilu800b@gmail.com>
2022-11-22 18:26:48 +0000

    01-factorial Completed

--- A-pass-tests/01-factorial/factorial.js
+++ A-pass-tests/01-factorial/factorial.js
@@ -8,6 +8,13 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+    // your code here
+    let result = 1;
+    for (let i = 1; i <= int; i++) {
+        result *= i;
+    }
+    return result;
+}
 
 module.exports = factorial;
